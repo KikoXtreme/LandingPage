f8f4d18f5be8b3eab39d602e5138f75326606407
Kiril Valkov <kiril_dgr@abv.bg>
2024-12-02 23:44:37 +0200

    getStarted update

--- src/components/GetStarted/GetStarted.tsx
+++ src/components/GetStarted/GetStarted.tsx
@@ -1,9 +1,13 @@
+import './getStarted.scss';
+
 export const GetStarted = () => {
     return (
-        <div>
-            <div>GetStarted</div>
-            <div>GetStarted</div>
-            <div>GetStarted</div>
+        <div className="getstarted-section">
+            <div className="box-wrapper">
+                <h2>Ready to Get started</h2>
+                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique sed scelerisque arcu scelerisque ultricies habitant ac semper lorem.</h3>
+                <button className="btn" onClick={() => (window.location.href = "#contact")}>Request a call</button>
+            </div>
         </div>
     )
 }
