985af4a4614faa4581c19525c762ef7118e2b68d
Kiril Valkov <kiril_dgr@abv.bg>
2024-12-04 16:06:58 +0200

    AgencyServices update

--- src/components/AgencyServices/AgencyServices.tsx
+++ src/components/AgencyServices/AgencyServices.tsx
@@ -1,9 +1,78 @@
+import { useState } from 'react';
+import './agencyServices.scss';
+
 export const AgencyServices = () => {
+    const [openArrow, setOpenArrow] = useState(false);
+
+    const toggleOpenArrow = () => {
+        setOpenArrow((prevState) => !prevState);
+        // console.log('openArrow', openArrow);
+    }
+
     return (
-        <div>
-            <div>AgencyServices</div>
-            <div>AgencyServices</div>
-            <div>AgencyServices</div>
+        <div className="agency-section">
+            <h2>Agency  Services</h2>
+            <div className="section-wrapper">
+                <img src="/assets/agencyservices-images/digiadds.png" alt="digiadds" />
+                <div>
+                    <div className="text">
+                        <div className='text-line'>
+                            <h3>Dynamic Digital Ads</h3>
+                            <img
+                                className='arrows'
+                                src={openArrow ? "/assets/icons/arrowup.svg" : "/assets/icons/arrowdown.svg"}
+                                width="40"
+                                height="20"
+                                alt="arrowdown"
+                                onClick={toggleOpenArrow}
+                            />
+                        </div>
+                        <p>Dynamic Digital Ads lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, amet sit dui quis amet gravida felis nibh. Netus erat id et placerat massa.</p>
+                    </div>
+                    <div className="text">
+                        <div className='text-line'>
+                            <h3>3D Visuals</h3>
+                            <img
+                                className='arrows'
+                                src={openArrow ? "/assets/icons/arrowup.svg" : "/assets/icons/arrowdown.svg"}
+                                width="40"
+                                height="20"
+                                alt="arrowdown"
+                                onClick={toggleOpenArrow}
+                            />
+                        </div>
+                        <p>3D Visuals lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, amet sit dui quis amet gravida felis nibh. Netus erat id et placerat massa.</p>
+                    </div>
+                    <div className="text">
+                        <div className='text-line'>
+                            <h3>Web Assets</h3>
+                            <img
+                                className='arrows'
+                                src={openArrow ? "/assets/icons/arrowup.svg" : "/assets/icons/arrowdown.svg"}
+                                width="40"
+                                height="20"
+                                alt="arrowdown"
+                                onClick={toggleOpenArrow}
+                            />
+                        </div>
+                        <p>Web Assets lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, amet sit dui quis amet gravida felis nibh. Netus erat id et placerat massa.</p>
+                    </div>
+                    <div className="text">
+                        <div className='text-line'>
+                            <h3>Corporate Identity</h3>
+                            <img
+                                className='arrows'
+                                src={openArrow ? "/assets/icons/arrowup.svg" : "/assets/icons/arrowdown.svg"}
+                                width="40"
+                                height="20"
+                                alt="arrowdown"
+                                onClick={toggleOpenArrow}
+                            />
+                        </div>
+                        <p>Corporate Identity lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, amet sit dui quis amet gravida felis nibh. Netus erat id et placerat massa.</p>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
