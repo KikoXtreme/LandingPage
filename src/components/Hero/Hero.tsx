7e4964b4a25810196e6a1c69a1443e540ba38e46
Kiril Valkov <kiril_dgr@abv.bg>
2024-12-04 12:15:50 +0200

    hero section update

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,10 +3,13 @@ import './hero.scss';
 export const Hero = () => {
     return (
         <div className="hero-section">
-            <div className="hero-wrapper">
-                <h2>Visuals Engineered <br />with Precision</h2>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, amet sit dui quis amet gravida felis nibh. Netus erat id et placerat massa.</h3>
-                <button className="btn" onClick={() => (window.location.href = "#contact")}>Request a call</button>
+            <div className="section-wrapper">
+                <img src="/assets/heroimg_670x500px_v1.png" alt="heroimg" />
+                <div className="text">
+                    <h1>Visuals Engineered <br />with Precision</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque, amet sit dui quis amet gravida felis nibh. Netus erat id et placerat massa.</p>
+                    <button className="btn" onClick={() => (window.location.href = "#contact")}>Request a call</button>
+                </div>
             </div>
         </div>
     );
