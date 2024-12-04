6022f806b7f65940d30178822640f8dacc5420e9
Kiril Valkov <kiril_dgr@abv.bg>
2024-12-04 11:24:40 +0200

    logo, footer update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,26 +4,32 @@ export const Footer = () => {
     return (
         <div id='contact' className="footer-section">
             <div className="one-column">
-                <h3>Studio</h3>
-                <h3>Our Work</h3>
-                <h3>Services</h3>
-                <h3>Information</h3>
+                <button onClick={() => (window.location.href = "/")}>Studio</button>
+                <button onClick={() => (window.location.href = "#work")}>Our Work</button>
+                <button onClick={() => (window.location.href = "#services")}>Services</button>
+                <button onClick={() => (window.location.href = "#information")}>Information</button>
                 <p>We are a digital design agency founded in 2004 in Sofia. We are all about the never ending journey of discovering, finding the best balance between form and function.</p>
             </div>
             <div className="one-column">
-                <h3>Terms & Conditions  </h3>
-                <h3>Privacy Policy </h3>
-                <h3>Contact Us</h3>
-                <h3>Cookie Preferences</h3>
+                <button onClick={() => (window.location.href = "#Terms&Conditions")}>Terms & Conditions  </button>
+                <button onClick={() => (window.location.href = "#PrivacyPolicy")}>Privacy Policy </button>
+                <button onClick={() => (window.location.href = "#ContactUs")}>Contact Us</button>
+                <button onClick={() => (window.location.href = "#CookiePreferences")}>Cookie Preferences</button>
             </div>
-            <div className="one-column">
-                <h3>Follow us:</h3>
+            <div className="one-column last">
+                <button>Follow us:</button>
                 <div className='social-networks'>
-                    <img src="/assets/icons/facebookicon.svg" alt="facebook" />
-                    <img src="/assets/icons/twittericon.svg" alt="twitter" />
-                    <img src="/assets/icons/linkedinicon.svg" alt="linkedin" />
+                    <a href="https://www.facebook.com/chromeye/" target="_blank" rel="noopener noreferrer">
+                        <img src="/assets/icons/facebookicon.svg" alt="facebook" />
+                    </a>
+                    <a href="https://twitter.com/chromeye" target="_blank" rel="noopener noreferrer">
+                        <img src="/assets/icons/twittericon.svg" alt="twitter" />
+                    </a>
+                    <a href="https://www.linkedin.com/company/chromeye/" target="_blank" rel="noopener noreferrer">
+                        <img src="/assets/icons/linkedinicon.svg" alt="linkedin" />
+                    </a>
                 </div>
-                <h3>Contact</h3>
+                <button onClick={() => (window.location.href = "#contact")}>Contact</button>
                 <p>
                     <span>
                         19, Dondukov blvd., Floor 6, Sofia<br />
